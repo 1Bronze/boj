a1b000fd68d93c38de122be4a0a688c4ba64373f
khdkkhdd <heneine@henesys.in>
2022-09-14 20:15:00 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -3,31 +3,33 @@ using namespace std;
 
 int main() {
     int num;
-    double highscore, mean;
-    
+    double highscore = 0, mean = 0;
+
     cin >> num;
-    
+
     double* arr = new double[num];
-    
+
+    cout.precision(5);
+
     for(size_t i = 0; i < num; i++) {
         cin >> arr[i];
     }
-    
+
     highscore = arr[0];
-    
+
     for(size_t i = 0; i < num; i++) {
         if(highscore < arr[i])
             highscore = arr[i];
     }
-    
+
     for(size_t i = 0; i < num; i++) {
         arr[i] = arr[i] / highscore * 100;
         mean += arr[i];
     }
-    
+
     mean /= num;
-    
+
     cout << mean;
-    
+
     delete[] arr;
 }
