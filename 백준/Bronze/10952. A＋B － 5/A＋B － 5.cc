e4168eba964708025b902b71d62073c4bf8de78c
khdkkhdd <heneine@henesys.in>
2022-09-14 18:46:22 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+    int a = 1, b = 1;
+
+    while (a != 0 && b != 0) {
+        cin >> a >> b;
+
+        if(a != 0 && b != 0)
+            cout << a + b << endl;
+    }
+}
