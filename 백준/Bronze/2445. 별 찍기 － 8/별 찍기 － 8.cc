cccf0606724aba1171c8bee650269ff8d9eb4375
khdkkhdd <heneine@henesys.in>
2022-12-20 23:23:42 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,21 @@
+  #include <stdio.h>
+
+  int getStarNum(int l, int N) {
+    if(l<=N) return l;
+    else return 2*N-l;
+  }
+
+  int getSpaceNum(int l, int N) {
+    return 2*N - 2*getStarNum(l, N);
+  }
+
+  int main() {
+    int cnt;
+    scanf("%d", &cnt);
+    for(int i=1; i<=2*cnt-1; i++) {
+        for(int j=0; j<getStarNum(i, cnt); j++) printf("*");
+        for(int j=0; j<getSpaceNum(i, cnt); j++) printf(" ");
+        for(int j=0; j<getStarNum(i, cnt); j++) printf("*");
+        printf("\n");
+    }
+  }
