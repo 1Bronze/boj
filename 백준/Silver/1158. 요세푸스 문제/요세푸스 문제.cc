fea7b6be291ea4274ca19884689be02eb84b34e8
Kim Handong <heneine@henesys.in>
2023-01-18 02:09:51 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 384 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int N, K;
+bool arr[5001];
+
+int main() {
+    cin >> N >> K;
+
+    for(int i=1; i<=5000; i++) {
+        if(i<=N) arr[i] = true;
+        else arr[i] = false;
+    }
+
+    cout << "<";
+
+    int target = K;
+    for(int i=1; i<=N; i++) {
+        if(arr[target]) {
+            arr[target] = false;
+            cout << target;
+            if(i!=N) cout << ", ";
+        } else {
+            for(int j=0; j<K; j++) {
+                target++;
+                if(target>N) target-=N;
+                if(!arr[target]) j--;
+            }
+            i--;
+        }
+    }
+
+    cout << ">";
+}
