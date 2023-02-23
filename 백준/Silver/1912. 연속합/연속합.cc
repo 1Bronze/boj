334abce15c6918884aa792cd7f705bc5d7ce77fd
Kim Handong <khdkkhdd@gmail.com>
2023-02-23 20:57:21 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,27 +1,30 @@
+/**
+ * 1912. 연속합
+ */
+
+#define MAX 100001
+
 #include <iostream>
 using namespace std;
 
-int arr[100000];
-int dp[100000];
-
-int max(int a, int b) { return (a>b) ? a:b; }
+int n, ans = -987654321;
+int arr[MAX];
+int DP[MAX];
 
 int main() {
-	int T; cin >> T;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	for(int i=0; i<T; i++)
-		cin >> arr[i];
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> arr[i];
 
-	dp[0] = arr[0];
+    for (int i = 1; i <= n; i++) {
 
-	for(int i=1; i<T; i++) {
-		if(dp[i-1]<0) dp[i] = arr[i];
-		else dp[i] = dp[i-1] + arr[i];
-	}
+        DP[i] = max(DP[i-1]+arr[i], arr[i]);
+        ans = max(ans, DP[i]);
+    }
 
-	int result = dp[0];
-	for(int i=1; i<T; i++)
-		result = max(result, dp[i]);
-
-	cout << result << endl;
+    cout << ans;
+    return 0;
 }
