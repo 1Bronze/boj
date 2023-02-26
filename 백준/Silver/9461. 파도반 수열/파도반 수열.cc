3a1437fd60c212b4b8d93f3c7cff2bef090dfabd
Kim Handong <khdkkhdd@gmail.com>
2023-02-26 22:54:47 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -1,26 +1,26 @@
+/**
+ * 9461. 파도반 수열
+ */
+
 #include <iostream>
-  using namespace std;
+using namespace std;
 
-  long long dp[101];
+int T, N;
+long long DP[101];
 
-  void execute() {
-    int num; cin >> num;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    for(int i=1; i<=num; i++) {
-        if(i==1) dp[1] = 1;
-        else if(i==2) dp[2] = 1;
-        else if(i==3) dp[3] = 1;
-        else if(i==4) dp[4] = 2;
-        else if(i==5) dp[5] = 2;
-        else dp[i] = dp[i-1] + dp[i-5];
+    DP[1] = 1, DP[2] = 1, DP[3] = 1, DP[4] = 2, DP[5] = 2;
+
+    for (int i = 6; i <= 100; i++)
+        DP[i] = DP[i-5] + DP[i-1];
+
+    cin >> T;
+    while(T--) {
+        cin >> N;
+        cout << DP[N] << "\n";
     }
-
-    cout << dp[num] << endl;
-  }
-
-  int main() {
-    int N; cin >> N;
-
-    for(int i=0; i<N; i++)
-        execute();
-  }
+}
