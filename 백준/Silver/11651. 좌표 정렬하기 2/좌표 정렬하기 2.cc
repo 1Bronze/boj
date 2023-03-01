c271404bfc3f53fab501d58d3483b111cc2cf0bb
Kim Handong <khdkkhdd@gmail.com>
2023-03-01 22:39:09 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 48 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -1,23 +1,41 @@
+/**
+ * 11651. 좌표 정렬하기 2
+ */
+
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+struct locate {
+    int x, y;
+};
+
 int N;
-vector<pair<int, int>> v;
+vector<locate> v;
+
+bool compare(locate a, locate b) {
+    if(a.y != b.y)
+        return (a.y < b.y);
+    else
+        return (a.x < b.x);
+}
 
 int main() {
-    int a; int b;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> a >> b;
-        v.push_back(pair<int, int>(b, a));
+    for (int i = 1; i <= N; i++) {
+        int x, y; cin >> x >> y;
+        v.push_back({x, y});
     }
 
-    sort(v.begin(), v.end());
+    sort(v.begin(), v.end(), compare);
 
-    for (int i = 0; i < N; i++) cout << v[i].second << " " << v[i].first << "\n";
+    for (int i = 0; i < v.size(); i++)
+        cout << v.at(i).x << " " << v.at(i).y << "\n";
 
     return 0;
 }
