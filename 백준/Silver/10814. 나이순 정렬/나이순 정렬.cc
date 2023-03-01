010b779e33ec717a2a2091ad707ada1219e64890
Kim Handong <khdkkhdd@gmail.com>
2023-03-01 22:44:18 +0900

    [Silver V] Title: 나이순 정렬, Time: 76 ms, Memory: 9852 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,28 +1,41 @@
+/**
+ * 10814. 나이순 정렬
+ */
+
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #include <string>
+#include <vector>
 using namespace std;
 
-int N;
-vector<pair<int, string>> v;
+struct member {
+    int age;
+    string name;
+};
 
-bool compare(pair<int, string> a, pair<int, string> b) {
-    return a.first < b.first;
+int N;
+vector<member> v;
+
+bool compare(member a, member b) {
+    return (a.age < b.age);
 }
 
 int main() {
-    int age; string name;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
     cin >> N;
-    for (int i = 0; i < N; i++) {
+    for (int i = 1; i <= N; i++) {
+        int age; string name;
         cin >> age >> name;
-        v.push_back(pair<int, string>(age, name));
+        v.push_back({age, name});
     }
 
     stable_sort(v.begin(), v.end(), compare);
 
-    for (int i = 0; i < N; i++) cout << v[i].first << " " << v[i].second << "\n";
+    for (int i = 0; i < N; i++)
+        cout << v.at(i).age << " " << v.at(i).name << "\n";
 
     return 0;
 }
