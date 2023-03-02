e43e2bf75ce0c05cfa328a9c3ae1759704154b60
Kim Handong <khdkkhdd@gmail.com>
2023-03-02 13:01:22 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1936 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,26 +1,30 @@
-#include <iostream>
+/**
+ * 10989. 수 정렬하기 3
+ */
 
+#define MAX 10001
+
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 int N;
-int arr[10001];
-
-bool compare(const int &a, const int &b) {
-    return a < b;
-}
+int arr[MAX];
 
 int main() {
-    scanf("%d", &N);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    int tmp;
+    cin >> N;
     for (int i = 0; i < N; i++) {
-        scanf("%d", &tmp);
+        int tmp; cin >> tmp;
         arr[tmp]++;
     }
-    
-    for (int i = 1; i < 10001; i++) {
-        while (arr[i]--) {
-            printf("%d\n", i);
-        }
+
+    for (int i = 1; i < MAX; i++) {
+        while(arr[i]--) cout << i << "\n";
     }
+
+    return 0;
 }
