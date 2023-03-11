e754e5b3824a4ee9a412622b7a9d79270277fb09
Kim Handong <khdkkhdd@gmail.com>
2023-03-11 10:00:36 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,72 +1,51 @@
-#define MAX_SIZE 10000
+/**
+ * 10845.  큐
+ */
 
 #include <iostream>
-#include <string>
+#include <queue>
 using namespace std;
 
-int s = 0;
-int front, back;
-int queue[MAX_SIZE];
-
-void push(int n) {
-    if(s == 0) {
-        front = 0; 
-        back = -1;
-    }
-    back = (++back) % MAX_SIZE;
-    queue[back] = n;
-    s++;
-}
-
-int pop() {
-	if(s == 0)
-	    return -1;
-	int returnIdx = front;
-	front = (++front) % MAX_SIZE;
-	s--;
-	return queue[returnIdx];
-}
-
-int getSize() {
-	return s;
-}
-
-int isEmpty() {
-	return (s == 0) ? 1:0;
-}
-
-int getFront() {
-    if(s == 0)
-	    return -1;
-    return queue[front];
-}
-
-int getBack() {
-    if(s == 0)
-	    return -1;
-    return queue[back];
-}
+int N;
+queue<int> q;
 
 int main() {
-	int cnt;
-	string ans;
-	cin >> cnt;
-	for(int i=0; i<cnt; i++) {
-		cin >> ans;
-		if(ans == "push") {
-			int n;
-			cin >> n;
-			push(n);
-		} else if (ans == "pop") {
-			cout << pop() << endl;
-		} else if (ans == "size") {
-			cout << getSize() << endl;
-		} else if (ans == "empty") {
-			cout << isEmpty() << endl;
-		} else if (ans == "front") {
-		    cout << getFront() << endl;
-		} else {
-		    cout << getBack() << endl;
-		}
-	}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    while(N--) {
+        string ans; cin >> ans;
+
+        if(ans=="push") {
+            int n; cin >> n;
+            q.push(n);
+        }
+        else if(ans=="pop") {
+            if(!q.empty()) {
+                cout << q.front() << "\n";
+                q.pop();
+            } else {
+                cout << "-1\n";
+            }
+        }
+        else if(ans=="size") {
+            cout << q.size() << "\n";
+        }
+        else if(ans=="empty") {
+            if(q.empty()) cout << "1\n";
+            else cout << "0\n";
+        }
+        else if(ans=="front") {
+            if(!q.empty()) cout << q.front() << "\n";
+            else cout << "-1\n";
+        }
+        else if(ans=="back") {
+            if(!q.empty()) cout << q.back() << "\n";
+            else cout << "-1\n";
+        }
+    }
+    
+    return 0;
 }
