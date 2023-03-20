ad71cc3cce4171c003e13a99225a007e67ca216d
Kim Handong <khdkkhdd@gmail.com>
2023-03-20 12:25:00 +0900

    [Silver I] Title: IOIOI, Time: 4 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -0,0 +1,45 @@
+/**
+ * 5525. IOIOI
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int N, M;
+string s;
+int cnt;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    string target = "I";
+    for (int i = 0; i < N; i++) {
+        target += 'O';
+        target += 'I';
+    }
+
+    cin >> M;
+    cin >> s;
+
+    for (int i = 0; i <= M-(2*N+1); i++) {
+        bool possible = true;
+
+        for (int j = i; j < i+(2*N+1); j++) {
+            char c = s[j];
+            if(i%2==0) {
+                if(j%2==0 && c=='O') possible = false;
+                if(j%2==1 && c=='I') possible = false;
+            } else {
+                if(j%2==1 && c=='O') possible = false;
+                if(j%2==0 && c=='I') possible = false;
+            }
+        }
+        if(possible) cnt++;
+    }
+
+    cout << cnt;
+}
