a8773abd56a7c37095cd40095d5f291f773d03ca
Kim Handong <khdkkhdd@gmail.com>
2023-03-20 12:34:13 +0900

    [Silver I] Title: IOIOI, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -28,14 +28,19 @@ int main() {
     for (int i = 0; i <= M-(2*N+1); i++) {
         bool possible = true;
 
+        if(s[i]=='O') continue;
+
         for (int j = i; j < i+(2*N+1); j++) {
             char c = s[j];
+
             if(i%2==0) {
                 if(j%2==0 && c=='O') possible = false;
                 if(j%2==1 && c=='I') possible = false;
+                if(!possible) break;
             } else {
                 if(j%2==1 && c=='O') possible = false;
                 if(j%2==0 && c=='I') possible = false;
+                if(!possible) break;
             }
         }
         if(possible) cnt++;
