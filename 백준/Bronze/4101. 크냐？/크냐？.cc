74751b22bd9fbce90c3517e6529f09d0ac33da1d
Kim Handong <khdkkhdd@gmail.com>
2023-04-01 14:27:23 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int a, b;
+    while(1) {
+        cin >> a >> b;
+        
+        if(a==0 && b==0) break;
+        
+        if (a > b) cout << "Yes\n";
+        else cout << "No\n";
+    }
+    
+    return 0;
+}
