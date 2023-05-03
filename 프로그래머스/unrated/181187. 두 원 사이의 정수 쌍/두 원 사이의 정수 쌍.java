e1e73c5e26e78410b881509923eb341646426c43
Kim Handong <khdkkhdd@gmail.com>
2023-05-03 16:24:50 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 17.57 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int r1, int r2) {
+        
+        long answer = 0;
+        
+        for(int i = 1; i <= r2; i++) {
+            
+            int lower = (int)Math.ceil(Math.sqrt((long)r1 * r1 - (long)i * i));
+            int upper = (int)Math.floor(Math.sqrt((long)r2 * r2 - (long)i * i));
+            
+            answer += upper - lower + 1;
+        }
+        
+        return answer * 4;
+    }
+}
