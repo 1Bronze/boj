e33934c294f6619dc437b91259e0d09cb5dac527
Kim Handong <khdkkhdd@gmail.com>
2023-05-20 17:23:12 +0900

    [Gold IV] Title: 거짓말, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.cc
+++ 백준/Gold/1043. 거짓말/거짓말.cc
@@ -1,7 +1,3 @@
-/**
- * 1043. 거짓말
- */
-
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -10,8 +6,9 @@ struct DisjointSet {
 
     vector<int> parent;
 
-    DisjointSet(int n): parent(n) {
-        for (int i = 0; i < n; i++) parent[i] = i;
+    DisjointSet(int n): parent(n+1) {
+        for (int i = 0; i < n+1; i++)
+            parent[i] = i;
     }
 
     int find(int u) {
@@ -22,52 +19,45 @@ struct DisjointSet {
     void merge(int u, int v) {
         u = find(u), v = find(v);
         if(u == v) return;
-        if(u < v) parent[v] = u;
-        else parent[u] = v;
+        if(u > v) parent[u] = v;
+        else parent[v] = u;
     }
 };
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
 
-    int N, M; cin >> N >> M;
-    DisjointSet sets(N+1);
+    int N, M, K;
+    cin >> N >> M;
 
-    int K; cin >> K;
-    // 진실을 아는 사람은 0 집합으로 분류
-    for (int i = 1; i <= K; i++) {
+    DisjointSet sets(N);
+    vector<int> party[M];
+
+    cin >> K;
+    for (int i = 0; i < K; i++) {
         int tmp; cin >> tmp;
         sets.parent[tmp] = 0;
     }
 
-    vector<vector<int>> party(M);
-    // party[i]: i번째 파티의 참가자들을 저장하는 벡터
     for (int i = 0; i < M; i++) {
-        int participants; cin >> participants;
-        for (int j = 0; j < participants; j++) {
-            int target; cin >> target;
-            party[i].push_back(target);
+        cin >> K;
+        for (int j = 0; j < K; j++) {
+            int tmp; cin >> tmp;
+            party[i].push_back(tmp);
         }
-        // 같은 파티 참가자는 같은 집합으로 설정
-        for (int j = 1; j < participants; j++)
+        for (int j = 1; j < party[i].size(); j++)
             sets.merge(party[i][0], party[i][j]);
     }
 
-    // 모든 파티에서 거짓말을 할 경우에서 시작
-    int answer = M;
 
-    // 각 파티마다 진실을 아는 사람이 있을 경우 거짓말을 할 수 없음.
-    // 이 경우 answer--
+    int result = M;
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < party[i].size(); j++) {
-            if(sets.find(party[i][j]) == 0) {
-                answer--;
+            if (sets.find(party[i][j]) == 0) {
+                result--;
                 break;
             }
         }
     }
 
-    cout << answer;
+    cout << result;
 }
